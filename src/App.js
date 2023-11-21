0bea4c384708d3c5ebaa56b2970e1445d90735f6
hhabiiba <hbmdhb.2003@gmail.com>
2023-11-21 12:15:22 -0800

    child comp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import BookList from './components/booklist';
 
 function App() {
   return (
     <>
    <h1>Favourite Books</h1>
    <p>Description</p>
+   <BookList/>
    </>
   );
 }
---
+++ src/components/booklist.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function BookList() {
+  return (
+    <div>
+       <ol>
+        <li>book 1</li>
+        <li>book 2</li>
+        <li>book 3</li>
+        </ol> 
+        
+            
+    </div>
+  )
+}
